0fddd9794c01418628ff59f88e4f7017a6e2d0c7
Alexandra <alexandraneff@live.com>
2022-11-02 21:04:02 -0500

    import components into app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import React from "react"
+import { Route, Switch } from "react-router-dom"
 import Bars from "./Bars"
+import Breweries from "./Breweries"
+import NavBar from "./NavBar"
+import SubmitForm from "./SubmitForm"
 
 
 function App() {
 return (
     <div>
-        <Bars />
+        <NavBar />
+        <Switch>
+            <Route exact path="/bars">
+                <Bars />
+            </Route>
+            <Route exact path="/breweries">
+                <Breweries />
+            </Route>
+            <Route exact path="/submitform">
+                <SubmitForm />
+            </Route>
+        </Switch>
     </div>
 )
 }
