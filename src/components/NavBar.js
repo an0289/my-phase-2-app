e9f84e572afb04741d9cffa9a7234c5a72250656
Alexandra <alexandraneff@live.com>
2022-11-02 21:10:16 -0500

    added NavLinks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,37 @@
 import React from "react"
+import {  NavLink } from "react-router-dom"
 
 function NavBar() {
+    return (
+        <div className="navbar">
+            <ul>
+            <li>
+            <NavLink 
+                to="/bars"
+                exact
+            >
+                Bars
+            </NavLink>
+            </li>
+            <li>
+            <NavLink 
+                to="/breweries"
+                exact
+            >
+                Breweries
+            </NavLink>
+            </li>
+            <li>
+            <NavLink 
+                to="/submitform"
+                exact
+            >
+                SubmitForm
+            </NavLink>
+            </li>
+            </ul>
+        </div>
+    )
 
 }
 
